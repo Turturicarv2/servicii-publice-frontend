69a0d5937ea4064dfa2e407679d0daa4f598ed33
Daniel Plamadeala <danielp@indsoft.ro>
2025-09-01 12:41:26 +0300

    add basic datatable

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import Button from "primevue/button"
+  import BonTable from "./views/BonTable.vue"
+</script>
 
 <template>
   <h2>You did it</h2>
+  <Button label="Save" icon="pi pi-check" iconPos="right" />
+
+  <BonTable />
 </template>
 
 <style scoped></style>
---
+++ src/views/bonTable.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import DataTable from 'primevue/datatable';
+    import Column from 'primevue/column';
+
+    const bonuri = [
+        {bon: 1, ghiseu: "G001", descriere: "lmao", stare: "preluat"},
+        {bon: 2, ghiseu: "G001", descriere: "lmao", stare: "preluat"},
+        {bon: 3, ghiseu: "G001", descriere: "lmao", stare: "preluat"},
+    ]
+</script>
+
+<template>
+    <DataTable :value=bonuri>
+        <Column field="bon", header="ID"></Column>
+        <Column field="ghiseu", header="Ghiseu"></Column>
+        <Column field="descriere", header="Descriere"></Column>
+        <Column field="stare", header="Stare"></Column>
+    </DataTable>
+</template>
+
+<style scoped></style>
